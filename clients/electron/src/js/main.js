2025da7fc4b87fa31736da19887515dfeea347e7
Tyler Travers <ttravers17@gmail.com>
2019-04-08 09:32:27 -0400

    some cleanup

--- clients/electron/src/js/main.js
+++ clients/electron/src/js/main.js
@@ -5,55 +5,77 @@ const ws = new WebSocket(`ws://localhost:${WEBSOCKET_PORT}`, {
     perMessageDeflate: false
 });
 
+let DATASTORE = {
+    CURRENT_IMAGE: {
+        buffer: new Uint8Array(),
+        mimetype: 'image/jpeg',
+        dataURL: '',
+        uploaded: false
+    },
+    CURRENT_TRANSFORM: {
+        buffer: new Uint8Array(),
+        mimetype: 'image/jpeg',
+        dataURL: ''
+    }
+}
+
 
 window.onload = function () {
+    // Grab all interactive elements
     const uploadBtn = document.getElementById("upload-btn")
     const uploadImage = document.getElementById("upload-image")
     const transformedImage = document.getElementById('transformed-image')
     const saveBtn = document.getElementById('save-btn')
     const transformBtn = document.getElementById('transform-btn')
 
+    // When message received update transform data and render image
     ws.on('message', async (data) => {
         console.log("Message received from websocket server", data)
-        // const dataURL = await blobToDataURL(new Blob([data], { type: 'image/jpg' })).catch(e => {
-        //     console.error("Error occured inr eceiver", e);
-        // })
-        let blob = new Blob([data], { type: 'image/jpg' });
-        const dataURL = await blobToDataURL(blob).catch(console.error)
+        const mimetype = 'image/jpeg'
+        const dataURL = await bufferToBase64(data, mimetype);
 
+        if (!dataURL) {
+            alert("An error occured processing image transform")
+            return;
+        }
+
+        DATASTORE.CURRENT_TRANSFORM.buffer = data
+        DATASTORE.CURRENT_TRANSFORM.mimetype = mimetype
+        DATASTORE.CURRENT_TRANSFORM.dataURL = dataURL
 
         transformedImage.src = dataURL
     })
 
+    // Upload button handles populating image and setting data to be sent
     uploadBtn.addEventListener('click', () => {
+        // Tell process to open filesystem
         ipcRenderer.send('openFile')
 
+        // User uploaded a file, process it
+        ipcRenderer.on('fileData', async (event, { data, mimetype }) => {
+            // turn the buffer into base64 string
+            const dataURL = await bufferToBase64(data, mimetype);
+            if (!dataURL) {
+                alert("An error occured processing image transform")
+                return;
+            }
 
-        ipcRenderer.on('fileData', async (event, fileData) => {
-            let blob = new Blob([fileData.data], { type: fileData.mimetype })
+            // Set our datastore to be used by transform
+            DATASTORE.CURRENT_IMAGE.buffer = data
+            DATASTORE.CURRENT_IMAGE.mimetype = mimetype
+            DATASTORE.CURRENT_IMAGE.dataURL = dataURL
+            DATASTORE.CURRENT_IMAGE.uploaded = true
 
-            const dataURL = await blobToDataURL(blob).catch((e) => {
-                alert('An error occured constructing image url from byte array', dataURL)
-                return ""
-            })
-
+            // render image
             uploadImage.src = dataURL
-
-            ws.send(fileData.data)
-
         })
     })
 
-    transformBtn.addEventListener('click', () => {
-        console.log("transform btn clicked")
-
-        const transform = 'crop';
-        const dataURI = transform + ',' + uploadImage.src;
-
-        ws.send(dataURI, (e) => {
-            console.error("Error occured sending data", e)
-        })
-
+    // Send transform to server/model for processing
+    transformBtn.addEventListener('click', async () => {
+        if (DATASTORE.CURRENT_IMAGE.uploaded) {
+            ws.send(DATASTORE.CURRENT_IMAGE.buffer);
+        }
     });
 
 
@@ -63,6 +85,15 @@ window.onload = function () {
             reader.onerror = reject;
             reader.onload = (e) => fulfill(reader.result);
             reader.readAsDataURL(blob);
+        })
+    }
+
+    function bufferToBase64(data, mimetype) {
+        let blob = new Blob([data], { type: mimetype })
+
+        return blobToDataURL(blob).catch((e) => {
+            console.error('An error occured constructing image url from byte array', e)
+            return ""
         })
     }
 }
--- server/server.py
+++ server/server.py
@@ -10,46 +10,74 @@ import io
 
 
 # TODO: use wss instead of ws
+import logging
+logger = logging.getLogger('websockets')
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 
-# Convert Image to Base64
-def im_2_b64(image):
-    buff = io.BytesIO()
-    image.convert('RGB')
-    image.save(buff, format="JPEG")
-    img_str = b64encode(buff.getvalue())
-    return img_str
+def transform_router(transform: str, image: any, **kwargs) -> Tuple[any, bool]:
+    def do_rotate(image, **kwargs):
+        return image.rotate(45)
+
+    def do_nothing(image, **kwargs):
+        return image
+
+    routes = {
+        'nothing': do_nothing,
+        'rotate90': do_rotate
+    }
+
+    transform_fn = routes.get(transform)
+    if not transform_fn:
+        print("This transform function doesn't exist")
+        return None, False
+
+    transform = transform_fn(image, **kwargs)
+
+    return transform, True
 
 
-def do_transform(transform: str, image) -> Tuple[Any, bool]:
-    return image.rotate(45), True
+def create_PIL_image_from_buffer(image_buffer):
+    image = Image.open(io.BytesIO(image_buffer))
+    converted_image = image.convert('RGB')
+    return converted_image
+
+
+def create_buffer_from_PIL_image(transformed_image):
+    buffer = io.BytesIO()
+    transformed_image.save(buffer, format='JPEG')
+    return buffer
 
 
 async def receive_image_transform_request(websocket: websockets.WebSocketCommonProtocol, path):
-    image_buffer = await websocket.recv()
-    print(f"Received image buffer")
+    while True:
+        # Wait to receive input from socket
+        image_buffer = await websocket.recv()
+        print("Received image buffer")
 
-    image_matrix = Image.open(io.BytesIO(image_buffer))
-    print("Array made from buffer")
+        image_matrix = create_PIL_image_from_buffer(image_buffer)
+        print("Array made from buffer")
 
-    # Do transform
-    transformed, ok = do_transform('rotate90', image_matrix)
-    # encoded = im_2_b64(transformed)
+        # Do transform
+        transformed, ok = transform_router('nothing', image_matrix)
 
-    return_buffer = io.BytesIO()
-    transformed.save(return_buffer, format='JPEG')
-
-    if ok:
-        await websocket.send(return_buffer.getvalue())
-
-    print("Completed image transformation and sent to client")
-
-start_server = websockets.serve(
-    receive_image_transform_request,
-    'localhost',
-    os.getenv("WEBSOCKET_PORT")
-)
+        if not ok:
+            print("An error occured")
+        else:
+            buffer = create_buffer_from_PIL_image(transformed)
+            await websocket.send(buffer.getvalue())
+            print("Completed image transformation and sent to client")
 
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    start_server = websockets.serve(
+        receive_image_transform_request,
+        'localhost',
+        os.getenv("WEBSOCKET_PORT")
+    )
+
+    print(
+        f"Starting up python socket server on port :{os.getenv('WEBSOCKET_PORT')}")
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
